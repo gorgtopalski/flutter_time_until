1ca0029357d766a0eb9abe0f6d14fed74a9b9837
Gueorgui Topalski <gueorgui@topalski.es>
2020-10-07 18:10:48 +0200

    provide reusable widgets and make use of the theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_time_until/time_calculator.dart';
 
+import 'common/theme.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -12,10 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Time Until',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: ApplicationTheme(isDark: false).themeData,
       home: MyHomePage(title: 'Time Until'),
       debugShowCheckedModeBanner: false,
     );
@@ -75,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
           child: _isSelected
-              ? Result(_now, _selected)
+              ? PageResult(TimeCalculator(_now, _selected, 4))
               : Text('Please select a date')),
       floatingActionButton: FloatingActionButton(
         onPressed: _showDatePicker,
@@ -86,93 +85,106 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class Result extends StatelessWidget {
-  final DateTime _now;
-  final DateTime _selected;
+class PageResult extends StatelessWidget {
+  final controller = PageController(initialPage: 0);
 
-  Result(this._now, this._selected);
+  final TimeCalculator _calc;
+
+  PageResult(this._calc);
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    return PageView(
+      controller: controller,
+      children: [
+        PageWidget(
+          title: 'Common',
+          titleIcon: Icons.query_builder,
+          entries: _calc.common,
+        ),
+        PageWidget(
+          title: 'Ancient',
+          titleIcon: Icons.public,
+          entries: _calc.ancient,
+        ),
+        PageWidget(
+          title: 'Scientifc',
+          titleIcon: Icons.biotech,
+          entries: _calc.scientific,
+        )
+      ],
+    );
+  }
+}
+
+class PageWidget extends StatelessWidget {
+  final String title;
+  final IconData titleIcon;
+  final Map<String, String> entries;
+
+  PageWidget({this.entries, this.title, this.titleIcon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
-            '${TimeCalc.diffInYears(_now, _selected)} years',
-            style: Theme.of(context).textTheme.headline2,
+          TitleWithEmoji(title: title, icon: titleIcon),
+          SizedBox(
+            height: 25,
           ),
-          Text(
-            '${TimeCalc.diffInMonths(_now, _selected)} months',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInWeeks(_now, _selected)} weeks',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInDays(_now, _selected)} days',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInHours(_now, _selected)} hours',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInMinutes(_now, _selected)} minutes',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInSeconds(_now, _selected)} seconds',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInMedievalMoment(_now, _selected)} moments',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInKe(_now, _selected)} ke',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInElectronicsJiffy(_now, _selected)} jiffies',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInPhysicsJiffy(_now, _selected)} jiffies',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInLustrum(_now, _selected)} lustrums',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInDecades(_now, _selected)} decades',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInCenturies(_now, _selected)} centuries',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInMillennia(_now, _selected)} millenia',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInMegannum(_now, _selected)} meganum',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInGalacticYear(_now, _selected)} galactic years',
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          Text(
-            '${TimeCalc.diffInPlankTime(_now, _selected)} plank time unit',
-            style: Theme.of(context).textTheme.headline2,
-          ),
+          DisplayEntries(entries)
         ],
-      ),
+      )),
+    );
+  }
+}
+
+class TitleWithEmoji extends StatelessWidget {
+  final String title;
+  final IconData icon;
+
+  TitleWithEmoji({this.title, this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Text(
+          title,
+          style: Theme.of(context).textTheme.headline1,
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Icon(icon,
+            color: Theme.of(context).textTheme.headline1.color,
+            size: Theme.of(context).textTheme.headline1.fontSize)
+      ],
+    );
+  }
+}
+
+class DisplayEntries extends StatelessWidget {
+  final Map<String, String> entries;
+
+  DisplayEntries(this.entries);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: entries.keys
+          .map((key) => Text(
+                entries[key] + ' ' + key,
+                style: Theme.of(context).textTheme.headline2,
+              ))
+          .toList(),
     );
   }
 }
