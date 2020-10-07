d7a0c8be2d62e37ea2430ce1623cf7eadeeab170
Gueorgui Topalski <gueorgui@topalski.es>
2020-10-07 20:12:23 +0200

    Popup menu that can change the precision of the values

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
+enum PopUpCommands { dark, increment, decrement }
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -35,6 +37,8 @@ class _MyHomePageState extends State<MyHomePage> {
   DateTime _now;
   DateTime _selected;
   bool _isSelected = false;
+  int _precision = 2;
+  bool _dark = false;
 
   _MyHomePageState() {
     _now = DateTime.now();
@@ -72,10 +76,73 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         leading: Icon(Icons.date_range),
         title: Text(widget.title),
+        actions: [
+          Builder(
+            builder: (context) => PopupMenuButton<PopUpCommands>(
+              onSelected: (PopUpCommands result) {
+                print(_precision);
+                switch (result) {
+                  case PopUpCommands.dark:
+                    setState(() {
+                      _dark = !_dark;
+                    });
+                    break;
+                  case PopUpCommands.increment:
+                    if (_precision < 5) {
+                      setState(() {
+                        _precision++;
+                      });
+                      Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text('Value precision is set to $_precision'),
+                        duration: Duration(seconds: 1),
+                      ));
+                    }
+                    break;
+                  case PopUpCommands.decrement:
+                    if (_precision > 1) {
+                      setState(() {
+                        _precision--;
+                      });
+                      Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text('Value precision is set to $_precision'),
+                        duration: Duration(seconds: 1),
+                      ));
+                    }
+                    break;
+                }
+              },
+              itemBuilder: (BuildContext context) =>
+                  <PopupMenuEntry<PopUpCommands>>[
+                PopupMenuItem<PopUpCommands>(
+                  enabled: _precision < 5,
+                  value: PopUpCommands.increment,
+                  child: ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text('Add precision'),
+                  ),
+                ),
+                PopupMenuItem<PopUpCommands>(
+                  enabled: _precision > 0,
+                  value: PopUpCommands.decrement,
+                  child: ListTile(
+                    leading: Icon(Icons.remove),
+                    title: Text('Remove precision'),
+                  ),
+                ),
+                PopupMenuDivider(),
+                CheckedPopupMenuItem<PopUpCommands>(
+                  value: PopUpCommands.dark,
+                  child: const Text('Dark Mode'),
+                  checked: _dark,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
       body: Center(
           child: _isSelected
-              ? PageBuilder(TimeCalculator(_now, _selected, 4))
+              ? PageBuilder(TimeCalculator(_now, _selected, _precision))
               : Text('Please select a date')),
       floatingActionButton: FloatingActionButton(
         onPressed: _showDatePicker,
