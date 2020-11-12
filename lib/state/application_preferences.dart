35741ed9046c7cc5f01d6192a889ac805a5b4d09
Gueorgui Topalski <gueorgui@topalski.es>
2020-11-12 09:30:09 +0100

    notify when values are loaded implement the update timer save changed values

--- lib/state/application_preferences.dart
+++ lib/state/application_preferences.dart
@@ -5,45 +5,46 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ApplicationPreferences extends ChangeNotifier {
   //Get stored preferences
-  SharedPreferences _prefs;
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
   int precision = 10;
   bool darkTheme = true;
   bool showTime = false;
   bool updateTimer = true;
-  DateTime now = DateTime.now();
+  bool _isLoaded = false;
+
+  bool get isLoaded => _isLoaded;
+
+  DateTime _now = DateTime.now();
+  DateTime get now => _now;
+
   Timer _timer;
 
   ApplicationPreferences() {
-    SharedPreferences.getInstance()
-        .then((value) => _prefs = value)
-        .whenComplete(() => _load());
+    _prefs.then((prefs) {
+      precision = prefs.getInt('precision') ?? 10;
+      darkTheme = prefs.getBool('darkTheme') ?? true;
+      showTime = prefs.getBool('showTime') ?? false;
+      updateTimer = prefs.getBool('updateTimer') ?? true;
+    }).whenComplete(() {
+      if (updateTimer) {
+        _timer = Timer.periodic(Duration(seconds: 1), _updateNow);
+      }
+    }).whenComplete(() {
+      _isLoaded = true;
+      notifyListeners();
+    });
   }
 
-  _load() {
-    precision = _prefs.getInt('precision') ?? 10;
-    darkTheme = _prefs.getBool('darkTheme') ?? true;
-    showTime = _prefs.getBool('showTime') ?? false;
-    updateTimer = _prefs.getBool('updateTimer') ?? true;
-
-    if (updateTimer) {
-      _timer = Timer.periodic(Duration(seconds: 1), updateCurrenTimeCallback);
-    }
-  }
-
-  void updateCurrentTime() {
-    now = DateTime.now();
+  void _updateNow(Timer t) {
+    _now = DateTime.now();
     notifyListeners();
-  }
-
-  void updateCurrenTimeCallback(Timer t) {
-    updateCurrentTime();
   }
 
   void showTimeSelection(bool toShow) {
     if (showTime != toShow) {
       showTime = toShow;
-      //_prefs.then((value) => value.setBool('showTimer', showTime));
+      _prefs.then((value) => value.setBool('showTimer', showTime));
       notifyListeners();
     }
   }
@@ -51,7 +52,7 @@ class ApplicationPreferences extends ChangeNotifier {
   void changePrecision(int precision) {
     if (this.precision != precision) {
       this.precision = precision;
-      //_prefs.then((value) => value.setInt('precision', this.precision));
+      _prefs.then((value) => value.setInt('precision', this.precision));
       notifyListeners();
     }
   }
@@ -59,24 +60,19 @@ class ApplicationPreferences extends ChangeNotifier {
   void changeTheme(bool theme) {
     if (darkTheme != theme) {
       this.darkTheme = theme;
-      //_prefs.then((value) => value.setBool('darkTheme', darkTheme));
+      _prefs.then((value) => value.setBool('darkTheme', darkTheme));
       notifyListeners();
     }
   }
 
-  void changeTimerUpdate(bool timer) {
-    if (updateTimer != timer) {
-      if (!updateTimer) {
-        if (_timer.isActive) {
-          _timer.cancel();
-        }
-      } else {
-        _timer = Timer.periodic(Duration(seconds: 1), updateCurrenTimeCallback);
-      }
-
-      updateTimer = timer;
-      //_prefs.then((value) => value.setBool('updateTimer', updateTimer));
-      notifyListeners();
+  void toggleTimerUpdate() {
+    updateTimer = !updateTimer;
+    _prefs.then((value) => value.setBool('updateTimer', updateTimer));
+    notifyListeners();
+    if (!updateTimer && _timer.isActive) {
+      _timer.cancel();
+    } else {
+      _timer = Timer.periodic(Duration(seconds: 1), _updateNow);
     }
   }
 }
