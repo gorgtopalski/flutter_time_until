d15ba8093c00430f9c2ea70c01ec6281be0de20c
Gueorgui Topalski <gueorgui@topalski.es>
2021-04-27 20:26:54 +0200

    change date entries display strings

--- lib/data/data_entry.dart
+++ lib/data/data_entry.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_time_until/time/time_format.dart';
 import 'package:hive/hive.dart';
 
 part 'data_entry.g.dart';
@@ -19,4 +20,9 @@ class DateEntry extends HiveObject {
   DateEntry.withIcon(this.description, this.date, this.icon);
 
   Duration get timeRemaining => date.difference(DateTime.now());
+
+  String get timeRemainingString =>
+      TimeFormat.formatDuration(this.timeRemaining);
+
+  String get timeSelectedString => TimeFormat.formatDate(this.date);
 }
